a48aaa89e50f9a47d53adeafba7e21a8a7ed0476
momen <momenmarwanh@gmail.com>
2025-01-22 15:04:33 +0200

    add simple rete limiting

--- src/main/java/gzg/momen/todolist/ToDoListApplication.java
+++ src/main/java/gzg/momen/todolist/ToDoListApplication.java
@@ -1,7 +1,10 @@
 package gzg.momen.todolist;
 
+import gzg.momen.todolist.filter.RateLimitingFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ToDoListApplication {
